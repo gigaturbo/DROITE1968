1a3b1557aea1c1361cc44a31c7f0ed22fa31bf77
camille-rouet <camille.rouet@protonmail.com>
2024-09-28 16:29:38 +0200

    ameliroation boite dialog

--- scripts/Jour1.gd
+++ scripts/Jour1.gd
@@ -14,10 +14,13 @@ func _process(delta):
 
 const lines: Array[String] = [
 	"Hiiiii, how are you?", 
-	"I am speaking alone",
-	"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et posuere magna. Nulla eget finibus nibh. Pellentesque libero elit, malesuada ac nisl a, pharetra bibendum ligula. Donec dapibus eget purus id convallis. Suspendisse lacus arcu, interdum ac ex sed, feugiat volutpat enim."
+	"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et posuere magna. Nulla eget finibus nibh. Pellentesque libero elit, malesuada ac nisl a, pharetra bibendum ligula. Donec dapibus eget purus id convallis. Suspendisse lacus arcu, interdum ac ex sed, feugiat volutpat enim.",
+	"I am speaking alone..."
 	]
 
 func _unhandled_input(event):
 	if event.is_action_pressed("left_click"):
-		DialogManager.start_dialog($TextPosition_corner.position, lines, Vector2(720,0))
+		DialogManager.start_dialog($TextPosition_corner.position, lines, Vector2(0,0))
+
+	if event.is_action_pressed("right_click"):
+		DialogManager.start_dialog($TextPosition_corner.position, lines, Vector2(720,200))
