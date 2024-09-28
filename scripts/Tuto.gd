a9307d18837be04b439353b2f67e945b72e13631
gigaturbo <11806309+gigaturbo@users.noreply.github.com>
2024-09-28 12:52:16 +0200

    Tuto area click and emit event

--- scripts/Tuto.gd
+++ scripts/Tuto.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+signal endTuto
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,3 +10,12 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+
+
+func _on_area_2d_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			if event.button_index == 1:
+				endTuto.emit()
