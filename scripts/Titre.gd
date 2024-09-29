8f668c50cd995bd7216769dc3a670cc4c9f07b2e
gigaturbo <11806309+gigaturbo@users.noreply.github.com>
2024-09-29 17:08:19 +0200

    interface menu

--- scripts/Titre.gd
+++ scripts/Titre.gd
@@ -3,43 +3,14 @@ extends Node2D
 @export var mainScene : PackedScene
 
 signal startButtonPressed
+signal creditButtonPressed
+signal quitterButtonPressed
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-
-
-func _on_texture_buttoncustom_pressed():
+func _on_start_pressed():
 	startButtonPressed.emit()
-	pass # Replace with function body.
 
+func _on_credits_pressed():
+	creditButtonPressed.emit()
 
-
-## a virer plus tard
-#func _unhandled_input(event):
-#	if event.is_action_pressed("A_button"):
-#
-#		DialogManager.start_dialog(Vector2(200, 300), 
-#			Vector2(500,150), 
-#			DialogManager.TextBoxTypes.REPONSE,
-#			["t'es qui ?"],
-#			0)
-#		DialogManager.buttonPressed.connect(reponse)
-#
-#		DialogManager2.start_dialog(Vector2(200, 300) + Vector2(0, 300), 
-#			Vector2(500,150), 
-#			DialogManager2.TextBoxTypes.REPONSE,
-#			["test ^^"],
-#			1)
-#		DialogManager2.buttonPressed.connect(reponse)
-#
-#
-#func reponse(oneDialogManager):
-#	print(oneDialogManager.text_box.text)
-#	print(oneDialogManager.id)
-#	DialogManager.inputCloseDialog()
-#	DialogManager2.inputCloseDialog()
-
-
+func _on_quitter_pressed():
+	quitterButtonPressed.emit()
