d18117ed03cc09c3fb282ac1fdf0ea401a17307c
camille-rouet <camille.rouet@protonmail.com>
2024-09-29 17:23:38 +0200

    musique fin

--- scripts/ScoreScreen.gd
+++ scripts/ScoreScreen.gd
@@ -13,12 +13,18 @@ func init(score, _scoreState):
 	$loose.hide()
 	$perfect.hide()
 	
+	get_node("../Musiques/Musique1Radio").stop()
+	get_node("../Musiques/Musique1").stop()
+	
 	if score < 6:
 		$loose.show()
+		get_node("../Musiques/MarseillaiseFluteBad").play()
 	elif score < 12:
 		$win.show()
+#		get_node("../Musiques/MarseillaisePianoGood").play()
 	elif score == 12:
 		$perfect.show()
+		get_node("../Musiques/MarseillaisePianoGood").play()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
