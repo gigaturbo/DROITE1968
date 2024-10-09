3cfe9fd6043e368015178e66e12da6a88209d12d
gigaturbo <11806309+gigaturbo@users.noreply.github.com>
2024-10-10 00:01:43 +0200

    FX assets tuto

--- scripts/Tuto.gd
+++ scripts/Tuto.gd
@@ -9,12 +9,21 @@ func _ready():
 	histState = -1
 
 func startTuto():
+	$tuto.hide()
+	$Bureau.show()
+	$contexte.show()
 	histState = 0
 
 func _input(event):
 	if histState == 0 and event is InputEventMouseButton:
 		if event.is_pressed():
-			if event.button_index == 1:
-				endTuto.emit()
-				histState = 2 # Avoid other tuto launch later
-						
+			$tuto.show()
+			$Bureau.hide()
+			$contexte.hide()
+			histState = 1
+
+
+func _on_jouer_pressed() -> void:
+	if histState == 1:
+		endTuto.emit()
+		histState = 2 # Avoid other tuto launch later
